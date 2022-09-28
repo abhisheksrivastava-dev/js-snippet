330269e59cb6dcdecc091ed9f4a102d8a6175b72
Abhishek Srivastava <74584768+abhisheksrivastava-dev@users.noreply.github.com>
2022-09-28 09:35:24 +0530

    scroll-animation

---
+++ index.js
@@ -0,0 +1,20 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes () {
+    // take the height of page random
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if(boxTop < triggerBottom){
+            box.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+    })
+}
